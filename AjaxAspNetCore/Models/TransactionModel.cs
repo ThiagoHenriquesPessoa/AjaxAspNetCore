43e85aed913084e96690943de12df69f7176bdbd
Thiago Henriques <thiagokxl5@gmail.com>
2021-11-25 07:59:49 -0300

    Adicionar arquivos de projeto.

---
+++ AjaxAspNetCore/Models/TransactionModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxAspNetCore.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "varchar(12)")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string bankname { get; set; }
+
+        [Column(TypeName = "varchar(11)")]
+        public string SWIFTCode { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
